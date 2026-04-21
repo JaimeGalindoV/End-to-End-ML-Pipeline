3e0b7ce527bbac132d83b07583eee33154be905b
Jaime Enrique Galindo Villegas <126115469+JaimeGalindoV@users.noreply.github.com>
2026-04-21 00:06:47 -0600

    Use environment variable for API port

--- src/app.py
+++ src/app.py
@@ -82,4 +82,5 @@ def predict(request: PredictionRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    API_PORT=int(os.getenv("API_PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=API_PORT)
